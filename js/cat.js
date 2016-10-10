81933f0f7e5028a8c4889657032b643b2a46979f
Jcook894 <Jcook894@icloud.com>
2016-10-09 22:40:07 -0400

    Need to stop constant appending.

--- js/cat.js
+++ js/cat.js
@@ -4,7 +4,7 @@ var kittyCounter = function (){
   $(document).ready( function(){
     $('#CatClicker').click(function(e) {
       var para = $("<p> Times you clicked the cat: "+ counter +"</p>");
-      $("#KittyCounter").append(para);
+      $("#KittyCounter").append(counter);
       alert("You clicked the cat! " + counter);
       counter++;
 
