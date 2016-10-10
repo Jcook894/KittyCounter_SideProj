e7be5be69412b4580b72d09699487c6fe8a2ef32
Jcook894 <Jcook894@icloud.com>
2016-10-09 22:33:09 -0400

    Appended the # of times you clicked the cat. Need to stop repetitive appending.

--- js/cat.js
+++ js/cat.js
@@ -3,6 +3,8 @@ var kittyCounter = function (){
 
   $(document).ready( function(){
     $('#CatClicker').click(function(e) {
+      var para = $("<p> Times you clicked the cat: "+ counter +"</p>");
+      $("#KittyCounter").append(para);
       alert("You clicked the cat! " + counter);
       counter++;
 
