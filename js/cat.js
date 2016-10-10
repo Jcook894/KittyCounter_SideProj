cb52b1d54a98d22215aef749477095e7a0a01d2f
Jcook894 <Jcook894@icloud.com>
2016-10-10 13:14:44 -0400

    Added new kitty to the page and gave it a counter as well.

--- js/cat.js
+++ js/cat.js
@@ -1,17 +1,30 @@
-var counter = 1
-var kittyCounter = function (){
+var kittySuit = function (){
+  var counter = 1
 
   $(document).ready( function(){
-    $('#CatClicker').click(function(e) {
+    $('#CatClicker1').click(function(e) {
       var para = $("<p> Times you clicked the cat: ");
       $("#KittyCounter").text(counter++);
       console.log("You clicked the cat! " + counter);
-      
 
     });
   });
 
+};
 
-}
+kittySuit();
 
-kittyCounter();
+var seriousCat = function(){
+  var counter = 1;
+  $(document).ready( function(){
+    $('#CatClicker2').click(function(e) {
+      var para = $("<p> Times you clicked the cat: ");
+      $("#KittyCounter1").text(counter++);
+      console.log("You clicked the cat! " + counter);
+
+    });
+  });
+
+};
+
+seriousCat();
