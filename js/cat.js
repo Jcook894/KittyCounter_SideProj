28945f5997da368c5344ce04e386c9eac04f1e3c
Jcook894 <Jcook894@icloud.com>
2016-10-09 22:16:55 -0400

    Made simple counter function that pops up in a alert message.

--- js/cat.js
+++ js/cat.js
@@ -1,5 +1,15 @@
-$(document).ready( function(){
-  $('#CatClicker').click(function(e) {
-    alert("You clicked the cat!");
+var counter = 1
+var kittyCounter = function (){
+
+  $(document).ready( function(){
+    $('#CatClicker').click(function(e) {
+      alert("You clicked the cat! " + counter);
+      counter++;
+
+    });
   });
-});
+
+
+}
+
+kittyCounter();
